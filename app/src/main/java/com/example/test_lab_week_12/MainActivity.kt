195075d3bec7f900fca0803de9d811e60b16cf35
edrichugo <hugoedric@gmail.com>
2025-11-30 17:13:35 +0700

    Commit No. 2 – Upgrading app with flow

--- app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test_lab_week_12.model.Movie
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
@@ -31,7 +35,6 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         })
-
         recyclerView.adapter = movieAdapter
 
         val movieRepository = (application as MovieApplication).movieRepository
@@ -43,20 +46,28 @@ class MainActivity : AppCompatActivity() {
                 }
             })[MovieViewModel::class.java]
 
-        movieViewModel.popularMovies.observe(this) { popularMovies ->
-            val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
-            movieAdapter.addMovies(
-                popularMovies
-                    .filter { movie ->
-                        movie.releaseDate?.startsWith(currentYear) == true
-                    }
-                    .sortedByDescending { it.popularity }
-            )
-        }
 
-        movieViewModel.error.observe(this) { error ->
-            if (error.isNotEmpty()) {
-                Snackbar.make(recyclerView, error, Snackbar.LENGTH_LONG).show()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                launch {
+                    movieViewModel.popularMovies.collect { movies ->
+                        val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
+                        movieAdapter.addMovies(
+                            movies.filter { movie ->
+                                movie.releaseDate?.startsWith(currentYear) == true
+                            }.sortedByDescending { it.popularity }
+                        )
+                    }
+                }
+
+                launch {
+                    movieViewModel.error.collect { error ->
+                        if (error.isNotEmpty()) {
+                            Snackbar.make(recyclerView, error, Snackbar.LENGTH_LONG).show()
+                        }
+                    }
+                }
             }
         }
     }
